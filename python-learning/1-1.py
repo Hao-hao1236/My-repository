33d7f92bbb7c88e17b9765d0c28cb84b9afd050b
Yang Zhanhao <3826144100@qq.com>
2026-01-29 11:15:36 +0000

    chore: add python-learning/1-1.py and remove 1.py

---
+++ python-learning/1-1.py
@@ -0,0 +1,9 @@
+# 需求：在控制台中输出"Hello, World!"和"Hello, Python!"字符串（# 表示注释，注释是给开发人员看的，Python 解释器会忽略注释 —— 快捷键：Ctrl + /）
+print("Hello, World!")
+print("Hello, Python!")
+# 运行方式：在终端中输入 python 文件名（如 python 1-1.py）即可运行该脚本
+# 注意：确保你已经安装了 Python 环境，并且在终端中可以使用 python 命令
+# 输出结果：
+# Hello, World!
+# Hello, Python!
+print("---------------------")  # 分割线
